d5a8fa3ebfd987e2f84428f85175f7eb51957b13
DaniilLing <136614439+DaniilLing@users.noreply.github.com>
2023-06-22 16:48:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2)
+{
+    System.Console.WriteLine($"{num1} меньше {num2}");
+}
+else
+{
+    System.Console.WriteLine($"{num2} меньше {num1}");
+}
